63661844f5e73cdbedeb1a9c6da4e1ed0a152df1
StefanoM98 <Utente@PC-STEFANO>
2023-09-22 15:32:14 +0200

    Videogame Aggiunto al database creato

--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -21,6 +21,41 @@
                 switch (opzioneSelezionata)
                 {
                     case 1:
+                        Console.WriteLine("Inserisci il nome del videogioco che vuoi inserire:");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Inserisci òla descrizione del videogioco che vuoi inserire:");
+                        string overview = Console.ReadLine();
+
+                        Console.WriteLine("Inserisci la data di rilascio del videogioco che vuoi inserire:");
+                        DateTime realease_date = DateTime.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Inserisci la software house del videogioco che vuoi inserire:");
+                        long software_house = long.Parse(Console.ReadLine());
+
+                        Videogioco nuovoVideogioco = new Videogioco()
+                        {
+                            Name = name,
+                            Overview = overview,
+                            Release_date = realease_date,
+                            Software_houseId = software_house
+                        };
+
+                        using (VideogameContext db = new VideogameContext())
+                        {
+                            try
+                            {
+                                db.Add(nuovoVideogioco);
+                                db.SaveChanges();
+
+                                Console.WriteLine("Il videogioco è stato aggiunto");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+
                         break;
                     case 2:
                         break;
