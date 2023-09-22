19e1fc8bf38b2f110ed2ef183dbcab0b253c5776
StefanoM98 <Utente@PC-STEFANO>
2023-09-22 15:49:59 +0200

    Ricerca videogame per ID

--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -58,14 +58,35 @@
 
                         break;
                     case 2:
+
+                        Console.WriteLine("Inserisci l'ID del gioco che vuoi trovare");
+                        long gameid = long.Parse(Console.ReadLine());
+
+                        using (VideogameContext db = new VideogameContext())
+                        {
+                            Videogioco videogioco = db.Videogiochi.FirstOrDefault(videogioco => videogioco.VideogameId == gameid);
+                            if (videogioco != null)
+                            {
+                                Console.WriteLine($"Nome: {videogioco.Name}");
+                                Console.WriteLine($"Descrizione: {videogioco.Overview}");
+                                Console.WriteLine($"Data di rilascio: {videogioco.Release_date}");
+                                Console.WriteLine($"Id della software house: {videogioco.Software_houseId}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Nessun videogioco presente nel database appartente a quel id...Se vuoi puoi contattate la casa produttrice e inserirlo all'interno del database con il tasto 1");
+                            }
+                        }
                         break;
                     case 3:
+
                         break;
                     case 4:
                         break;
                     case 5:
                         break;
                     case 6:
+                        return;
                         break;
                 }
             }
